7d32eb79950be4b7fb1198546124d128e3091f8c
AnhHuy <tranhuy27396@gmail.com>
2018-09-05 20:41:09 +0700

    commit and push

--- app/src/main/java/com/example/huy/demobridgeapp/DuLieuDauVao_ThietKe_Fragment.java
+++ app/src/main/java/com/example/huy/demobridgeapp/DuLieuDauVao_ThietKe_Fragment.java
@@ -7,9 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -17,9 +20,11 @@ import java.util.ArrayList;
 import static com.example.huy.demobridgeapp.R.layout.dulieudauvao_thietke_fragment;
 
 public class DuLieuDauVao_ThietKe_Fragment extends Fragment {
+    EditText edtTenDuAn;
 
 
     public DuLieuDauVao_ThietKe_Fragment (){
+
 
     }
     @Nullable
@@ -31,16 +36,35 @@ public class DuLieuDauVao_ThietKe_Fragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
        // TextView textView = view.findViewById(R.id.tvDemo);
+
+         addControls();
+         addEvents();
 
 
         // ánh xạ spinner
         Spinner spinnerDanhSach = view.findViewById(R.id.spinner);
-        ArrayList<String> arrayDanhSach= new ArrayList<String>();
+        final ArrayList<String> arrayDanhSach= new ArrayList<String>();
         arrayDanhSach.add("Dầm thép liên hợp");
         arrayDanhSach.add("Dầm thép không liên hợp");
-        ArrayAdapter arrayAdapter = new ArrayAdapter(getContext(), R.layout.dulieudauvao_thietke_fragment, arrayDanhSach);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1,  arrayDanhSach);
         spinnerDanhSach.setAdapter(arrayAdapter);
+        // sự kiện
+   }
+    public void tinhToan (View v){
+        String edtenDuAn = edtTenDuAn.getText().toString();
+
+
+    }
+
+    private void addControls() {
+        edtTenDuAn = getView().findViewById(R.id.edtTenDuAn);
+
+
+
+    }
+    private void addEvents() {
 
     }
 
